1858f3c3ac6af6eccbaa2729f44bdbefbabbd5e6
nguyenvietanh1409 <vietanh20030914@gmail.com>
2022-05-04 14:21:20 +0700

    DemoTinhKeThua

---
+++ src/DemoTinhKeThua/TestRectangular.java
@@ -0,0 +1,19 @@
+package DemoTinhKeThua;
+
+public class TestRectangular {
+    public static void main(String[] args){
+        Rectangular rec1 = new Rectangular();
+        System.out.println("Width: "+ rec1.getWidth());
+        System.out.println("Length: "+ rec1.getLength());
+        System.out.println("Height: "+ rec1.getHeight());
+        System.out.println("Area: "+ rec1.getArea());
+        System.out.println("Volume: "+ rec1.getVolume());
+
+        Rectangular rec2 = new Rectangular();
+        System.out.println("Width: "+ rec2.getWidth());
+        System.out.println("Length: "+ rec2.getLength());
+        System.out.println("Height: "+ rec2.getHeight());
+        System.out.println("Area: "+ rec2.getArea());
+        System.out.println("Volume: "+ rec2.getVolume());
+    }
+}
